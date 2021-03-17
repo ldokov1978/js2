622c994491b17ffc3b36cba6e05dc3ced4e56f97
Denis <ldokov@mail.ru>
2021-03-17 23:46:57 +0300

    Упрощена запись ф-ии, добавлены знач. по умолчанию

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ const goods = [
   { title: 'Socks', price: 50 },
   { title: 'Jacket', price: 350 },
   { title: 'Shoes', price: 250 },
+  {},
 ];
 
-const renderGoodsItem = (title, price) => {
-  return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+const renderGoodsItem = (title='Jeans', price=500) => {
+  return `<div class="goods-item"><h3>${title}</h3><p>&euro; ${price}</p></div>`;
 };
 
 const renderGoodsList = (list) => {
